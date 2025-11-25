f92eea0bb15577d8b1a2aef4ca02bc4636f4c531
alforuete <aruete2015@gmail.com>
2025-11-25 19:41:19 -0300

    Fix balance

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -26,11 +26,11 @@ module.exports = {
   },
   networks: {
     hardhat: {
-      chainId: 31337,
+      chainId: 1337,
     },
     localhost: {
       url: "http://127.0.0.1:8545",
-      chainId: 31337,
+      chainId: 1337,
     },
     // red local de ganache
     ganache: {
